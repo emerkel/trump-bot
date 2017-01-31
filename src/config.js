8b8be013940e50e8b53eba0c9e52d7f66b8d7a39
Eddie Merkel <eddie.merkel@neeralu.com>
2017-01-31 21:53:23 +0000

    make it better

---
+++ src/config.js
@@ -0,0 +1,24 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+// Uncomment for when deploying to Heroku
+
+// module.exports = {
+//   consumer_key: process.env.CONSUMER_KEY,
+//   consumer_secret: process.env.CONSUMER_SECRET,
+//   access_token: process.env.ACCESS_TOKEN,
+//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+// };
+
+module.exports = {
+  consumer_key: 'rJcuup0p2AwT88kmLuFpeVSSc',
+  consumer_secret: 'T41mvPnVLyinXEKNrsR9TqRuAqgHHC1Zaap6h28x3FhaHqEJoF',
+  access_token: '5697562-ZSPMBGVnRwMBbKpuUvq2jmOTZZVX0PSlRXuYMUTW6R',
+  access_token_secret: 'Pn6dam2MCClKGx3tDjt6HDIhyh1WeIFh02h1ehy2thnD3',
+};
+
+
