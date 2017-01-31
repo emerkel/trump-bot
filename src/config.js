ea6b27416005fd058d15477a17f813431c822dbf
Eddie Merkel <eddie.merkel@neeralu.com>
2017-01-31 22:00:54 +0000

    add environment variables

--- src/config.js
+++ src/config.js
@@ -7,18 +7,18 @@
 
 // Uncomment for when deploying to Heroku
 
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
-
 module.exports = {
-  consumer_key: 'rJcuup0p2AwT88kmLuFpeVSSc',
-  consumer_secret: 'T41mvPnVLyinXEKNrsR9TqRuAqgHHC1Zaap6h28x3FhaHqEJoF',
-  access_token: '5697562-ZSPMBGVnRwMBbKpuUvq2jmOTZZVX0PSlRXuYMUTW6R',
-  access_token_secret: 'Pn6dam2MCClKGx3tDjt6HDIhyh1WeIFh02h1ehy2thnD3',
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
+
+// module.exports = {
+//   consumer_key: 'rJcuup0p2AwT88kmLuFpeVSSc',
+//   consumer_secret: 'T41mvPnVLyinXEKNrsR9TqRuAqgHHC1Zaap6h28x3FhaHqEJoF',
+//   access_token: '5697562-ZSPMBGVnRwMBbKpuUvq2jmOTZZVX0PSlRXuYMUTW6R',
+//   access_token_secret: 'Pn6dam2MCClKGx3tDjt6HDIhyh1WeIFh02h1ehy2thnD3',
+// };
 
 
