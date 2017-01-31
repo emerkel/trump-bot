2f92ceb76d275ee173df343fb93f3a04a376a83b
Eddie Merkel <eddie.merkel@neeralu.com>
2017-01-31 22:05:21 +0000

    changed rt times from 5 to 25 minutes

--- src/bot.js
+++ src/bot.js
@@ -7,8 +7,8 @@ var strings = require('./helpers/strings')
 var Twitter = new twit(config)
 
 // Frequency in minutes
-var retweetFrequency = 5
-var favoriteFrequency = 5
+var retweetFrequency = 25
+var favoriteFrequency = 25
 
 // RANDOM QUERY STRING  =========================
 
